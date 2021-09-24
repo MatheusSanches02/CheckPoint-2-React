a0300a46507be09f5acaa199611d4f351be22bec
danielle <RM88913@fiap.com.br>
2021-09-23 23:29:22 -0300

    alteraçao styled

---
+++ exercicio5-2/src/components/rodape/styledRodape.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ElHtml = styled.html`
+    footer{
+        background-color: var(--primary);
+        color: var(--white);
+        font-family: 'Regular';
+        width: 100%
+        font-size: 32em;
+        min-width: 48em
+    }
+    
+`
