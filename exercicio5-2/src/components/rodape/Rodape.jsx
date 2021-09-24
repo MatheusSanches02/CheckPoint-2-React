d35103d7e726622817912047691ba360ccf2db2a
danielle <RM88913@fiap.com.br>
2021-09-23 23:26:39 -0300

    Arquivo jsx criado

---
+++ exercicio5-2/src/components/rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styledRodape'
+
+export default function Rodape(){
+    return (
+        <div>
+            <footer id='rodape' background-color>
+                <p>Rua Caetano de Araújo, 112 - Nova Iguaçu - RJ, 97431-090</p>
+            </footer>
+        </div>
+
+    )
+}
