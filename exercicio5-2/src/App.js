01170484dbc490205966b69a273fbad85fb9f57c
giovannafranca <giovannafrancamagalhaes@gmail.com>
2021-09-23 23:04:17 -0300

    adicionando styled e estilo no cabecalho

--- exercicio5-2/src/App.js
+++ exercicio5-2/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import './assets/css/style.css'
+import Cabecalho from './components/cabecalho/Cabecalho';
 
 function App() {
   return (
     <div className="App">
-      
+        <Cabecalho/>
     </div>
   );
 }
--- exercicio5-2/src/components/cabecalho/styledCabecalho.js
+++ exercicio5-2/src/components/cabecalho/styledCabecalho.js
@@ -1,1 +1,20 @@
 import styled from 'styled-components';
+
+export const ElHtml = styled.html`
+    header{
+        background-color: var(--primary);
+        width: 100%;
+        height: 70px;
+    }
+    h1{
+        color:var(--white);
+        padding : 6px;
+        font-size: 2em;
+    }
+    p{
+        margin-left: 10px;
+        margin-top: 5px;
+        color: var(--black-light)
+    }
+    
+`
